61a3baf1215be62957104149d8f760afc84192de
likaifei <li0kaifei@126.com>
2018-05-21 17:26:18 +0800

    finish

--- public/common.js
+++ public/common.js
@@ -5,10 +5,13 @@ layui.use(['element','layer', 'form'], function(){
         template: '\
         <li v-bind:lay-id="name">\
         {{ name }}\
-        <button v-on:click="$emit(\'remove\')">X</button>\
+        <span title="ttl" class="layui-badge layui-bg-gray">{{ ttl }}</span>\
+        <span title="size" class="layui-badge layui-bg-gray">{{ size }}</span>\
+        <span v-on:click="$emit(\'refresh\')">&nbsp;<i class="layui-icon layui-icon-refresh-1"></i></span>\
+        <span title="remove" class="layui-badge layui-bg-gray" v-on:click="$emit(\'remove\')">X</span>\
         </li>\
         ',
-        props: ['name']
+        props: ['name','size','ttl']
     })
     Vue.component('key-info', {
         template: '\
@@ -32,7 +35,7 @@ layui.use(['element','layer', 'form'], function(){
         </li>\
         ',
         props: ['name']
-    })// <button v-on:click="$emit(\'remove\')">X</button>\
+    })
 
     var cmdData = {
         cmd : '',
@@ -42,8 +45,6 @@ layui.use(['element','layer', 'form'], function(){
         newkey: '',
         keys: [
             {
-                type: 'hash',
-                name: 'Do the dishes',
             },
         ],
     }
@@ -54,7 +55,7 @@ layui.use(['element','layer', 'form'], function(){
 
 
     function cmd(method, args){
-        if(typeof(args) != 'array') args = [args]
+        if(typeof(args) != 'object') args = [args]
         cmdData.result += method + ' ' + args.join(' ') + '\n'
         return axios.post('/', {
             method: method,
@@ -62,7 +63,7 @@ layui.use(['element','layer', 'form'], function(){
         })
     }
 
-    function showKey(key, method, args){
+    function showKey(key, type, method, args){
         cmd(method, args).then((r)=>{
             if(typeof(r.data.data)=='string')r.data.data = [r.data.data]
             var index = -1
@@ -76,13 +77,17 @@ layui.use(['element','layer', 'form'], function(){
                 keyData.keys.push( {
                 "name": key,
                 "data": r.data.data,
-                "type": 'string'
+                "type": 'string',
+                "ttl": '--',
+                "size": '--'
             })
             }else{
                 Vue.set(keyData.keys, index, {
                 "name": key,
                 "data": r.data.data,
-                "type": 'string'
+                "type": 'string',
+                "ttl": '--',
+                "size": '--'
             })
             }
             function later(filter, key){
@@ -90,8 +95,50 @@ layui.use(['element','layer', 'form'], function(){
                     element.tabChange('tab', key)
                 }
             }
+            ttl(key, type)
             setTimeout(later('tab', key),300)
         }).catch((e)=>{layer.msg(e.data)})
+    }
+
+    function ttl(key, type){
+        var index
+        cmd('ttl', key).then((r)=>{
+            if(r.data.success){
+                setData(key,'ttl',r.data.data)
+            }
+        })
+        if(type == 'list'){
+            cmd('llen', key).then((r)=>{
+                if(r.data.success){
+                    setData(key,'size',r.data.data)
+                }
+            })
+        }else if(type == 'zset'){
+            cmd('zcard', key).then((r)=>{
+                if(r.data.success){
+                    setData(key,'size',r.data.data)
+                }
+            })
+        }else if(type == 'hash'){
+            cmd('hlen', key).then((r)=>{
+                if(r.data.success){
+                    setData(key,'size',r.data.data)
+                }
+            })
+        }
+    }
+
+    function setData(key, method, value){
+        var index = findIndex(key)
+        if(key != -1) keyData.keys[index][method] = value
+    }
+    function findIndex(key){
+        for(var i = 0; i < keyData.keys.length; i++){
+            if(keyData.keys[i].name == key){
+                return i
+            }
+        }
+        return -1
     }
 
     function getKey(key, type = 'none'){
@@ -105,13 +152,13 @@ layui.use(['element','layer', 'form'], function(){
             })
         }else{
            if(type == 'string'){
-            showKey(key, 'get', key)
+            showKey(key, type, 'get', key)
            }else if(type == 'list'){
-            showKey(key, 'lrange', [key, 0, 99])
+            showKey(key, type, 'lrange', [key, 0, 99])
            }else if(type == 'hash'){
-            showKey(key, 'hgetall', key)
+            showKey(key, type, 'hgetall', key)
            }else if(type == 'zset'){
-            showKey(key, 'zrange', [key, 0, 99, 'WITHSCORES'])
+            showKey(key, type, 'zrange', [key, 0, 99, 'WITHSCORES'])
            }else{
                layer.msg('unknow type: ' + type)
            }
@@ -135,6 +182,7 @@ layui.use(['element','layer', 'form'], function(){
         })
     } 
 
+    //命令框
     new Vue({
         el: '#exec',
         data:cmdData,
@@ -157,13 +205,18 @@ layui.use(['element','layer', 'form'], function(){
         }
     })
 
-
+    //信息选项卡
     new Vue({
         el: '#mainTab',
         data:keyData,
         methods: {
+            refresh: function(key){
+                getKey(key)
+            }
         }
     })
+    
+    //数据库选择加刷新
     new Vue({
         el: '#naav',
         data:{
@@ -175,7 +228,10 @@ layui.use(['element','layer', 'form'], function(){
             changeDB: function(db){
                 this.showSub = false
                 cmd('select', db).then((r)=>{
-                    if(r.data.success) return layer.msg('切换成功')
+                    if(r.data.success) {
+                        refreshKeys()
+                        return layer.msg('切换成功')
+                    }
                     layer.msg('切换失败: ' + r.data.msg)
                 }).catch((e)=>{
                     layer.msg(e.data)
@@ -184,20 +240,13 @@ layui.use(['element','layer', 'form'], function(){
         }
     })
     
+    //KEY列表
     new Vue({
         el: '#key-list',
         data:keyList,
         methods: {
-            addNewKey: function () {
-                cmd('setnx', [this.newkey, 'empty']).then((r)=>{
-                    if(r.data.success)refreshKeys()
-                    layer.msg(r.data.msg)
-                }).catch((e)=>{layer.msg(e.data)})
-                this.newkey = ''
-            },
             refreshKeys: refreshKeys,
             getKey: getKey,
-            
             remove: function(key){
                 cmd('del', key).then((r)=>{
                     if(r.data.success)return refreshKeys()
